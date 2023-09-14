8071e85651f64717ffccbf29ab841dc3d4e22ca8
Wai Yan Zaw Tin <recoverywaiyan19@gmail.com>
2023-09-15 07:08:25 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import "./App.css";
+
 import "font-awesome/css/font-awesome.min.css";
+
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
 import Projects from "./components/Projects";

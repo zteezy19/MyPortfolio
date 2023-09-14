afc130e4ab2988cee06e7df2a96a5d599adffb8e
Wai Yan Zaw Tin <recoverywaiyan19@gmail.com>
2023-09-15 06:14:01 +0800

    Added Hackerrank Icon Hover Effect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,25 +12,34 @@ const Header = () => {
       </div>
       <div className="social-media-icons">
         <a
+          href="https://www.hackerrank.com/waiyanzawtin01"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon className="hackerrank-icon" icon={faHackerrank} />
+        </a>
+        <a
           href="https://www.linkedin.com/in/wai-yan-zaw-tin/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <i className="fa fa-linkedin-square" aria-hidden="true"></i>
         </a>
-        <a
-          href="https://www.hackerrank.com/waiyanzawtin01"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faHackerrank} />
-        </a>
+
         <a
           href="https://github.com/zteezy19"
           target="_blank"
           rel="noopener noreferrer"
         >
           <i className="fa fa-github" aria-hidden="true"></i>
+        </a>
+
+        <a
+          href="mailto:your-email@example.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa fa-envelope" aria-hidden="true"></i>
         </a>
       </div>
     </header>

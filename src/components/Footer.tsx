a22fe4dd43a3e6560df6548b54c2e788d73e6dcd
Wai Yan Zaw Tin <recoverywaiyan19@gmail.com>
2023-09-18 03:20:31 +0800

    Changed MyPicture.jpeg & added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,44 @@
-const Footer = () => {
-  return <div id="contact">{/* Your skills content */}</div>;
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faInstagram,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+import "./Footer.css";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <p className="gradient-text">ztz.dev</p>
+      <div className="social-media-icons">
+        <a
+          href="https://twitter.com/waiyanzt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a
+          href="https://instagram.com/waiyanzawtin"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a
+          href="https://www.facebook.com/waiyan.zawtin.1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+      </div>
+      <p className="copyright">
+        Copyright © 2023, Your Name. All rights reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;

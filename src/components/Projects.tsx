8994ea0705df59b8116de82ac593d06ad9042af1
Wai Yan Zaw Tin <recoverywaiyan19@gmail.com>
2023-09-18 16:40:44 +0800

    Made changes to Projects.tsx and css

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -54,9 +54,12 @@ const Projects = () => {
             </a>
           </span>
         </div>
-        {/* Add VibeMix as the second project */}
+        {/* VibeMix Project */}
         <div className="terminal-project-title">
-          <span>VibeMix: A Discord Bot</span>
+          <span>
+            VibeMix: A Discord Bot for Sentiment Analysis and Music
+            Recommendations
+          </span>
         </div>
         <div>
           <span className="terminal-prompt">~$</span>
@@ -65,16 +68,47 @@ const Projects = () => {
         <div>
           <span className="terminal-prompt">~$</span>
           <span>
-            Technologies Used:
-            <span className="terminal-tech"> Python</span>,
-            <span className="terminal-tech"> Discord.py</span>
+            Technologies Used:<span className="terminal-tech"> Python</span>,
+            <span className="terminal-tech"> Discord.py</span>,
+            <span className="terminal-tech"> TextBlob</span>
           </span>
         </div>
         <div>
           <span className="terminal-prompt">~$</span>
           <span>Status: Ongoing</span>
         </div>
-        {/* Add more projects here */}
+        <div>
+          <span className="terminal-prompt">~$</span>
+          <span>
+            Description: VibeMix is a multi-functional Discord bot designed to
+            elevate your server's atmosphere. It leverages NLP algorithms to
+            perform sentiment analysis on chat data and subsequently recommends
+            music playlists or tracks that align with the overall mood.
+          </span>
+        </div>
+        <div>
+          <span className="terminal-prompt">~$</span>
+          <span>Features: Sentiment Analysis, Music Recommendations</span>
+        </div>
+        <div>
+          <span className="terminal-prompt">~$</span>
+          <span>
+            GitHub:{" "}
+            <a
+              href="https://github.com/zteezy19/VibeMix"
+              target="_blank"
+              rel="noreferrer"
+              className="terminal-link"
+            >
+              View the code here.
+            </a>
+          </span>
+        </div>
+
+        {/* More Coming Soon */}
+        <div className="terminal-project-title">
+          <span>More Coming Soon..</span>
+        </div>
       </div>
     </div>
   );
